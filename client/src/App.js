069d2831285ad96abc3044912a812e8f8f0b4dd7
Alex Wilson <fonzmp@gmail.com>
2019-07-27 04:24:43 -0400

    remove unecessary

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./shared/home";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <NavBar />
       <div className="contentBody">
         <React.Fragment>
           <Switch>
