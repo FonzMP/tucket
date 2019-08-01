06b7f2a03afbf3e2a785cb67a425747fe14a922a
fonzmp <fonzmp@gmail.com[color]>
2019-07-31 22:58:44 -0500

    added if null to not show

--- client/src/components/shared/navigation/navbar.js
+++ client/src/components/shared/navigation/navbar.js
@@ -4,11 +4,10 @@ import { NavLink } from "react-router-dom";
 class Navbar extends Component {
   renderLinks = () => {
     return this.props.links.map((link, index) => {
-      return (
+      return link.display !== null ?
         <NavLink exact to={link.href} key={index} className="nav-link" activeClassName="active">
           {link.display}
-        </NavLink>
-      );
+        </NavLink> : null
     });
   };
   render() {
