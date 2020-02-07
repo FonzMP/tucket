35bf6445faf9dface4fec1e1a4e130ade51debb1
Alex Wilson <fonzmp@gmail.com>
2020-02-06 21:33:47 -0600

    .env and login logic

--- client/src/components/shared/auth/login.js
+++ client/src/components/shared/auth/login.js
@@ -5,12 +5,14 @@ import { AuthServices } from "../../../services/auth.service";
 import { NavContext } from "../navigation/navContext";
 
 function Login({ history }) {
+  const initialLogin = { username: "", password: "" };
   const context = useContext(AuthContext);
   const navContext = useContext(NavContext);
-  const [user, setUser] = useState({ username: "", password: "" });
+  const [user, setUser] = useState(initialLogin);
   const [showError, setShowError] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
   const [loggedIn, setLoggedIn] = useState(false);
+  const tester = new RegExp(/^[a-zA-Z0-9]{8,14}$/);
 
   useEffect(() => {
     const checkState = history.location.state;
@@ -21,31 +23,39 @@ function Login({ history }) {
     }
   }, [history.location.state, navContext]);
   function loginUser() {
-    AuthServices.loginUser(user)
-      .then(resp => resp.json())
-      .then(response => {
-        if (!!response.error) {
-          setErrorMsg(response.error);
-          setShowError(true);
-          setUser({ username: "", password: "" });
-        } else {
-          setLoggedIn(true);
-          context.setUser(response.user);
-          AuthServices.setStorage(response.user);
-        }
-      })
-      .catch(err => console.log("error here in login", err));
+    if (
+      (user.username.match(tester) && user.password.match(tester)) ||
+      user.username.toLowerCase() === process.env.REACT_APP_ADMIN_USERNAME
+    ) {
+      AuthServices.loginUser(user)
+        .then(resp => resp.json())
+        .then(response => {
+          if (!!response.error) {
+            setErrorMsg(response.error);
+            setShowError(true);
+            setUser(initialLogin);
+          } else {
+            setLoggedIn(true);
+            context.setUser(response.user);
+            AuthServices.setStorage(response.user);
+          }
+        })
+        .catch(err => console.log("error here in login", err));
+    } else {
+      setErrorMsg(
+        "Username and password must be 8-14 chars and consist of letters and numbers only."
+      );
+      setShowError(true);
+    }
   }
 
   function handleOnChange(e) {
-    if (
-      user.username.length > 0 &&
-      user.password.length > 0 &&
-      e.key === "Enter"
-    ) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
+  function handleKeyUp(e) {
+    if (e.key === "Enter") {
       loginUser();
-    } else {
-      setUser({ ...user, [e.target.name]: e.target.value });
     }
   }
 
@@ -61,13 +71,15 @@ function Login({ history }) {
           name="username"
           value={user.username}
           onChange={e => handleOnChange(e)}
+          onKeyUp={e => handleKeyUp(e)}
         />
         <label htmlFor="password">Password:</label>
         <input
           type="password"
           name="password"
-          defaultValue={user.password}
-          onKeyUp={e => handleOnChange(e)}
+          value={user.password}
+          onChange={e => handleOnChange(e)}
+          onKeyUp={e => handleKeyUp(e)}
         />
         <div className="mock-button" onClick={() => loginUser()}>
           Login
