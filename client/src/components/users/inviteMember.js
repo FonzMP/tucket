c8622fd867f9d9034ecccb31c6ebaed8f70be52f
Alex Wilson <fonzmp@gmail.com>
2020-03-03 23:21:12 -0600

    setup invite search onChange

--- client/src/components/users/inviteMember.js
+++ client/src/components/users/inviteMember.js
@@ -1,27 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import UserService from "../../services/user.service";
 
 function InviteMember({ match }) {
   const [userSearch, setUserSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [errorDisplay, setErrorDisplay] = useState("");
+  let interval;
+  useEffect(() => {
+    const timer = setTimeout(() => search(), 500);
+    return () => clearTimeout(timer);
+  }, [userSearch]);
 
-  function searchMembers() {
+  function search() {
+    setErrorDisplay("");
+    if (userSearch.length == 0) {
+      setSearchResults([]);
+      return;
+    }
+    setSearchResults([]);
     UserService.searchUsers(userSearch.toLowerCase())
       .then(resp => resp.json())
       .then(resp => {
+        if (resp.users.length == 0) {
+          setErrorDisplay("No users found");
+        }
         setSearchResults(resp.users);
+        clearInterval(interval);
       })
       .catch(err => {
         console.log("error getting user with that name", err);
+        clearInterval(interval);
       });
   }
 
   function renderResults() {
     return searchResults.map(user => {
       return (
-        <div key={user._id}>
-          <span>{user.username}</span>
-          <button onClick={() => addUser(user._id)}>Add</button>
+        <div key={user._id} className="project-item-wrap">
+          <span className="projectButtonWrap">
+            <span>{user.username}</span>
+            <button onClick={() => addUser(user._id)}>Add</button>
+          </span>
         </div>
       );
     });
@@ -44,11 +63,19 @@ function InviteMember({ match }) {
     <div>
       <h1>Invite Member</h1>
       <label htmlFor="username">Username</label>
-      <input type="text" onChange={e => setUserSearch(e.target.value)} />
-      <div>
-        <button onClick={() => searchMembers()}>Search Users</button>
-      </div>
-      {searchResults.length > 0 ? renderResults() : null}
+      <input
+        type="text"
+        onChange={e => {
+          setUserSearch(e.target.value);
+        }}
+      />
+      {/* <div>
+        <button onChange={() => searchMembers()}>Search Users</button>
+      </div> */}
+      {searchResults.length > 0 ? (
+        <div className="invite-results-block">{renderResults()}</div>
+      ) : null}
+      {errorDisplay ? <div>{errorDisplay}</div> : null}
     </div>
   );
 }
