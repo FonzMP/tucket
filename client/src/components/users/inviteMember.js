19e5f59541732469980f3b7b332c6e8c0d007b41
Alex Wilson <fonzmp@gmail.com>
2020-06-11 14:12:17 -0500

    quick filter on adding user

--- client/src/components/users/inviteMember.js
+++ client/src/components/users/inviteMember.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from "react";
 import UserService from "../../services/user.service";
+import ProjectServices from "../../services/project.service";
 
 function InviteMember({ match }) {
   const [userSearch, setUserSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [errorDisplay, setErrorDisplay] = useState("");
+  const [project, setProject] = useState({});
   let interval;
   useEffect(() => {
     const timer = setTimeout(() => search(), 500);
     return () => clearTimeout(timer);
   }, [userSearch]);
 
+  useEffect(() => {
+    ProjectServices.getProject(match.params.projectId)
+      .then((resp) => resp.json())
+      .then((response) => {
+        setProject(response.project);
+      });
+  }, [match]);
   function search() {
     setErrorDisplay("");
     if (userSearch.length === 0) {
@@ -19,27 +28,44 @@ function InviteMember({ match }) {
     }
     setSearchResults([]);
     UserService.searchUsers(userSearch.toLowerCase())
-      .then(resp => resp.json())
-      .then(resp => {
+      .then((resp) => resp.json())
+      .then((resp) => {
         if (resp.users.length === 0) {
           setErrorDisplay("No users found");
         }
         setSearchResults(resp.users);
         clearInterval(interval);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log("error getting user with that name", err);
         clearInterval(interval);
       });
   }
 
+  function checkProject(userId) {
+    if (project.owner._id == userId) {
+      return true;
+    }
+    if (project.invited.includes(userId)) {
+      return true;
+    }
+    if (project.members.includes(userId)) {
+      return true;
+    }
+    return false;
+  }
+
   function renderResults() {
-    return searchResults.map(user => {
+    return searchResults.map((user) => {
       return (
         <div key={user._id} className="project-item-wrap">
           <span className="projectButtonWrap">
             <span>{user.username}</span>
-            <button onClick={() => addUser(user._id)}>Add</button>
+            {checkProject(user._id) ? (
+              <span class="mock-button">Already added</span>
+            ) : (
+              <button onClick={() => addUser(user._id)}>Add</button>
+            )}
           </span>
         </div>
       );
@@ -49,11 +75,11 @@ function InviteMember({ match }) {
   function addUser(userId) {
     let projectId = match.params.projectId;
     UserService.inviteUserToProject(projectId, userId)
-      .then(resp => resp.json())
-      .then(response =>
+      .then((resp) => resp.json())
+      .then((response) =>
         console.log("successfully added user to the project ", response)
       )
-      .catch(err =>
+      .catch((err) =>
         console.log(
           "error adding user to project " + projectId + "with id " + userId
         )
@@ -65,7 +91,7 @@ function InviteMember({ match }) {
       <label htmlFor="username">Username</label>
       <input
         type="text"
-        onChange={e => {
+        onChange={(e) => {
           setUserSearch(e.target.value);
         }}
       />
